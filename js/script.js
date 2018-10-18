639686cddb53c37cb95700af94b291ec4e0b9ab0
jawakar <jawakarsmith@gmail.com>
2018-10-19 00:32:28 +0530

    goin to location

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
             //Change mobile-click
   $(".menu-icon i").on("click", function() {
                   $("nav ul").toggleClass("showing");
-                  $(".outer-nav li a i").remove();
             });
 
             //Act on scrolling
